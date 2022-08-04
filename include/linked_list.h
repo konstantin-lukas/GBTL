41a6b4e0371366d60f9ab008b57df5c8801fff8d
konstantin-lukas <konstantin_lukas@web.de>
2022-08-04 20:25:55 +0200

    initial commit; set up project directory

---
+++ include/linked_list.h
@@ -0,0 +1,14 @@
+//
+// Created by Konstantin on 2022/08/04.
+//
+
+#ifndef GBSTL_LINKED_LIST_H
+#define GBSTL_LINKED_LIST_H
+
+
+class LinkedList {
+
+};
+
+
+#endif //GBSTL_LINKED_LIST_H
---
+++ source/linked_list.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Konstantin on 2022/08/04.
+//
+
+#include "../include/linked_list.h"
