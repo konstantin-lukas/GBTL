54ef7da1db78abe86ffc0165634cf5fa2558ca27
Konstantin Lukas <konstantin_lukas@web.de>
2022-08-04 23:04:54 +0200

    began implementation of linked list data structure

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,14 +1,73 @@
-//
-// Created by Konstantin on 2022/08/04.
-//
-
 #ifndef GBSTL_LINKED_LIST_H
 #define GBSTL_LINKED_LIST_H
 
+#include <cstddef>
+#include <iostream>
 
-class LinkedList {
+namespace gbstl {
 
-};
+    template<typename type> class LinkedList {
+        struct Link {
+            type value;
+            Link* nextLink;
+            explicit Link(type val) : value(val), nextLink(nullptr){}
+        };
+    private:
+        Link* m_FirstLink;
+        Link* m_currentLink;
+        size_t m_Size;
+    public:
+        LinkedList() : m_FirstLink(nullptr), m_currentLink(nullptr), m_Size(0) {}
+        type& getElementAt(unsigned int index) {
+            m_currentLink = m_FirstLink;
+            if (m_currentLink == nullptr) throw std::out_of_range("linked list is empty");
+            if (index >= m_Size) throw std::out_of_range(std::string("index ") + std::to_string(index) + std::string(" is out of range"));
+            for (int i = 0; i < index; i++) {
+                m_currentLink = m_currentLink->nextLink;
+            }
+            return m_currentLink->value;
+        }
+        type& operator[](unsigned int index) {
+            return getElementAt(index);
+        }
+        const type& operator[](unsigned int index) const {
+            return getElementAt(index);
+        }
+        ~LinkedList() {
+            Link* garbage = m_FirstLink;
+            while (garbage != nullptr) {
+                Link* nextLink = garbage->nextLink;
+                delete garbage;
+                garbage = nextLink;
+            }
+        }
+        void push(type value) {
+            if (m_FirstLink == nullptr) {
+                m_FirstLink = new Link(value);
+            } else {
+                m_currentLink = m_FirstLink;
+                while (m_currentLink->nextLink != nullptr)
+                    m_currentLink = m_currentLink->nextLink;
+                m_currentLink->nextLink = new Link(value);
+            }
+            this->m_Size++;
+        }
+        // TODO: REMOVE ELEMENT AT
+        // TODO: POP
+        // TODO: COPY CONSTRUCTOR
+        // TODO: EMPTY
+        // TODO: CONSTRUCTOR FROM ARRAY
+        void print() {
+            m_currentLink = m_FirstLink;
+            while (m_currentLink != nullptr) {
+                std::cout << m_currentLink->value;
+                if (m_currentLink->nextLink != nullptr) std::cout << ", ";
+                m_currentLink = m_currentLink->nextLink;
+            }
+            std::cout << std::endl;
+        }
+        size_t length() { return m_Size; }
+    };
 
-
+}
 #endif //GBSTL_LINKED_LIST_H
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "linked_list.h"
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    gbstl::LinkedList<int> list;
+    list.push(6);
+    list.push(3);
+    list.push(81);
+    list.push(8511);
+    list.print();
+    list[1] = 52;
+    list.print();
+    list.getElementAt(3) = 1;
+    list.print();
+    std::cout << list[1] << std::endl;
 }
