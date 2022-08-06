9880f39bb635a7197afdea8cdbd699e763f8f4a0
Konstantin Lukas <konstantin_lukas@web.de>
2022-08-06 11:46:11 +0200

    reverted to raw pointers for linked list and overloaded assignment operator for move

--- include/linked_list.h
+++ include/linked_list.h
@@ -10,61 +10,86 @@ namespace gbstl {
     template<typename type> class LinkedList {
         struct Link {
             type value;
-            std::shared_ptr<Link>nextLink;
+            Link *nextLink;
             explicit Link(type val) : value(val), nextLink(nullptr){}
-            explicit Link(type val, std::shared_ptr<Link> next) : value(val), nextLink(next){}
+            explicit Link(type val, Link *next) : value(val), nextLink(next){}
         };
     private:
-        std::shared_ptr<Link>mp_FirstLink;
-        std::shared_ptr<Link>mp_currentLink;
-        size_t m_Length;
+        Link *mp_Head;
+        Link *mp_InternalPtr;
+        size_t m_Length = 0;
     public:
         // CONSTRUCTOR
-        LinkedList() : mp_FirstLink(nullptr), mp_currentLink(nullptr), m_Length(0) {}
+        LinkedList() : mp_Head(nullptr), mp_InternalPtr(nullptr) {};
 
         // COPY CONSTRUCTOR
-        LinkedList(const LinkedList<type> &src) {
+        LinkedList(const LinkedList &src) {
             m_Length = src.m_Length;
-            mp_FirstLink = std::shared_ptr<Link>(new Link(src.mp_FirstLink->value, src.mp_FirstLink->nextLink));
-            mp_currentLink = mp_FirstLink;
-            while (mp_currentLink->nextLink != nullptr) {
-                mp_currentLink->nextLink = std::shared_ptr<Link>(new Link(mp_currentLink->nextLink->value, mp_currentLink->nextLink->nextLink));
-                if (mp_currentLink->nextLink != nullptr) mp_currentLink = mp_currentLink->nextLink;
+            mp_Head = new Link(src.mp_Head->value, src.mp_Head->nextLink);
+            mp_InternalPtr = mp_Head;
+            while (mp_InternalPtr->nextLink != nullptr) {
+                mp_InternalPtr->nextLink = new Link(mp_InternalPtr->nextLink->value, mp_InternalPtr->nextLink->nextLink);
+                if (mp_InternalPtr->nextLink != nullptr) mp_InternalPtr = mp_InternalPtr->nextLink;
             }
         }
 
         // MOVE CONSTRUCTOR
-        LinkedList(const LinkedList<type> &&src) noexcept {
-            mp_FirstLink = src.mp_FirstLink;
-            mp_currentLink = mp_FirstLink;
+        LinkedList(LinkedList &&src) noexcept {
+            mp_Head = src.mp_Head;
+            mp_InternalPtr = mp_Head;
             m_Length = src.m_Length;
+            src.mp_Head = nullptr;
+            src.mp_InternalPtr = nullptr;
+            src.m_Length = 0;
         }
-        type& getLinkRefAt(unsigned int index) {
-            mp_currentLink = mp_FirstLink;
-            if (mp_currentLink == nullptr) throw std::out_of_range("linked list is empty");
+
+        LinkedList& operator=(LinkedList &&src) noexcept {
+            if (this != &src) {
+                mp_Head = src.mp_Head;
+                mp_InternalPtr = mp_Head;
+                m_Length = src.m_Length;
+                src.mp_Head = nullptr;
+                src.mp_InternalPtr = nullptr;
+                src.m_Length = 0;
+            }
+            return *this;
+        }
+
+        // DESTRUCTOR
+        ~LinkedList() {
+            Link* garbage = mp_Head;
+            while (garbage != nullptr) {
+                Link* nextLink = garbage->nextLink;
+                delete garbage;
+                garbage = nextLink;
+            }
+        }
+        type& getLinkAt(unsigned int index) {
+            mp_InternalPtr = mp_Head;
+            if (mp_InternalPtr == nullptr) throw std::out_of_range("linked list is empty");
             if (index >= m_Length) throw std::out_of_range(std::string("index ") + std::to_string(index) + std::string(" is out of range"));
             for (int i = 0; i < index; i++) {
-                mp_currentLink = mp_currentLink->nextLink;
+                mp_InternalPtr = mp_InternalPtr->nextLink;
             }
-            return mp_currentLink->value;
+            return mp_InternalPtr->value;
         }
-        type getLinkAt(unsigned int index) {
-            return getLinkRefAt(index);
-        }
-        type& operator[](unsigned int index) {
-            return getLinkRefAt(index);
-        }
-        type operator[](unsigned int index) const {
+        const type& getLinkAt(unsigned int index) const {
             return getLinkAt(index);
         }
-        void append(type value) {
-            if (mp_FirstLink == nullptr) {
-                mp_FirstLink = std::shared_ptr<Link>(new Link(value));
+        type& operator[](unsigned int index) {
+            return getLinkAt(index);
+        }
+        const type& operator[](unsigned int index) const {
+            return getLinkAt(index);
+        }
+        void append(const type& value) {
+            if (mp_Head == nullptr) {
+                mp_Head = new Link(value);
             } else {
-                mp_currentLink = mp_FirstLink;
-                while (mp_currentLink->nextLink != nullptr)
-                    mp_currentLink = mp_currentLink->nextLink;
-                mp_currentLink->nextLink = std::shared_ptr<Link>(new Link(value));
+                mp_InternalPtr = mp_Head;
+                while (mp_InternalPtr->nextLink != nullptr)
+                    mp_InternalPtr = mp_InternalPtr->nextLink;
+                mp_InternalPtr->nextLink = new Link(value);
             }
             this->m_Length++;
         }
@@ -76,6 +101,7 @@ namespace gbstl {
         // TODO: DOUBLY LINKED LIST
         // TODO: GET INDEX OF
         // TODO: COUNT
+        // TODO: TAIL MEMBER
         // TODO INSERT
         // TODO: APPENDARRAY
         // TODO: INSERTARRAY
@@ -84,11 +110,11 @@ namespace gbstl {
         // TODO: OPTIMIZE RUN TIME BY CHECKING IF CURRENT LINK IS USABLE FOR FUNCTION
         // TODO: INITIALIZER LIST
 
-        size_t length() { return m_Length; }
+        [[nodiscard]] size_t length() const { return m_Length; }
 
         class Iterator {
         public:
-            explicit Iterator(std::shared_ptr<Link> ptr) : mp_Ptr(ptr) {}
+            explicit Iterator(Link *ptr) : mp_Ptr(ptr) {}
             Iterator& operator++() {
                 mp_Ptr = mp_Ptr->nextLink;
                 return *this;
@@ -108,11 +134,11 @@ namespace gbstl {
                 return mp_Ptr == it.mp_Ptr;
             }
         private:
-            std::shared_ptr<Link> mp_Ptr;
+            Link *mp_Ptr;
         };
 
         Iterator begin() {
-            return Iterator(mp_FirstLink);
+            return Iterator(mp_Head);
         }
 
         Iterator end() {
--- test/test.cpp
+++ test/test.cpp
@@ -3,14 +3,18 @@
 #include <memory>
 int main() {
     gbstl::LinkedList<int> list1;
+
     list1.append(1);
     list1.append(2);
     list1.append(3);
     gbstl::LinkedList<int> list2 = std::move(list1);
-    list2.append(4);
-    list2.append(5);
-    std::cout << "Length: " << list2.length() << ", Size: " << sizeof list2 << std::endl;
-    for (int value : list2) {
+    gbstl::LinkedList<int> list3;
+    list2.append(4);;
+    list3 = std::move(list2);
+    list3.append(5);
+    list3[1] = list3[2];
+    std::cout << "Length: " << list3.length() << ", Size: " << sizeof list3 << std::endl;
+    for (int value : list3) {
         printf("---%d\n", value);
     }
 }
